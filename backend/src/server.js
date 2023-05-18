49c31583b78ec90cda35121913a47be20acf70a9
Caio Espindola <caioespindola@hotmail.com>
2023-05-18 09:32:01 -0300

    test connect aws

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ const app = require('./app')
 /* Método para poder utilizar as variáveis do ambiente .env*/
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3306
 
 app.listen(PORT, () => console.log(`servidor esta rodando na porta ${PORT}`))
